0b683849b6d68cbb119398c85475b70c99f09910
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-03-04 19:52:14 -0600

    Update administrator icons

--- Classes/Menu/Console.cs
+++ Classes/Menu/Console.cs
@@ -640,10 +640,6 @@ namespace Seralyth.Classes.Menu
                         adminConeObject.transform.position = playerRig.headMesh.transform.position + playerRig.headMesh.transform.up * (GetIndicatorDistance(playerRig) * playerRig.scaleFactor);
 
                         adminConeObject.transform.LookAt(GorillaTagger.Instance.headCollider.transform.position);
-
-                        Vector3 rot = adminConeObject.transform.rotation.eulerAngles;
-                        rot += new Vector3(0f, 0f, Mathf.Sin(Time.time * 2f) * 10f);
-                        adminConeObject.transform.rotation = Quaternion.Euler(rot);
                     }
 
                     // Admin serversided scale
